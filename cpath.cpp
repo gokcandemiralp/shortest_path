3cd06c5c84a24041a37a87003eca27d80cd21d6d
Gökcan Demiralp <gok.can.13@gmail.com>
2021-08-03 23:53:04 +0300

    fixed the closest() function

--- cpath.cpp
+++ cpath.cpp
@@ -13,18 +13,18 @@ vector<Point> closest(vector<Point> vec) {
 	int temp;
 	Point tempPoint;
 	vector<Point>::iterator swapment;
-	vector<Point>::iterator i = vec.begin();
-	for (vector<Point>::iterator j = i; j != vec.end() && (j + 1) != vec.end(); ++j) {
-		vector<Point>::iterator a = i;
-		vector<Point>::iterator b = (j+1);
-		temp = (((*a).x - (*b).x) * ((*a).x - (*b).x)) + (((*a).y - (*b).y) * ((*a).y - (*b).y));
-		if (smallest > temp) { smallest = temp; swapment = b; }
+	for (vector<Point>::iterator i = vec.begin(); i != vec.end() && (i + 1) != vec.end() ; ++i, smallest = INT_MAX) {
+		for (vector<Point>::iterator j = i; j != vec.end() && (j + 1) != vec.end(); ++j) {
+			vector<Point>::iterator a = i;
+			vector<Point>::iterator b = (j + 1);
+			temp = (((*a).x - (*b).x) * ((*a).x - (*b).x)) + (((*a).y - (*b).y) * ((*a).y - (*b).y));
+			if (smallest > temp) { smallest = temp; swapment = b; }
+		}
+		if ((i + 1) != vec.end()) {
+			tempPoint = *(i + 1);
+			*(i + 1) = *swapment;
+			*swapment = tempPoint;
+		}
 	}
-	if ((i + 1) != vec.end()) {
-		tempPoint = *(i + 1);
-		*(i + 1) = *swapment;
-		*swapment = tempPoint;
-	}
-	
 	return vec;
 }
