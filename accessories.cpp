62894bbb2b66131cb97296e5e13731cee0311c95
Gökcan Demiralp <gok.can.13@gmail.com>
2021-08-05 23:06:39 +0300

    totalDistance() added to calculate optimization

--- accessories.cpp
+++ accessories.cpp
@@ -1,5 +1,17 @@
-//het_gokcandemiralp 04-08-21
+//het_gokcandemiralp 05-08-21
 #include "accessories.h"
+
+float totalDistance(vector<Point> vec){
+	Point b;
+	Point a = Point(0, 0);
+	float ans = 0;
+	for (vector<Point>::iterator i = vec.begin(); i != vec.end(); ++i) {
+		b = *i;
+		ans += sqrt(((a.x - b.x) * (a.x - b.x)) + ((a.y - b.y) * (a.y - b.y)));
+		a = b;
+	}
+	return ans;
+}
 
 vector<Point> randomPoints(int low,int up,int n) {
 	vector<Point> vec;
