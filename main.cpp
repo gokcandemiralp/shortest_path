347e44afeeedd28a060d701ba357468dae600707
Gökcan Demiralp <gok.can.13@gmail.com>
2021-08-09 12:23:15 +0300

    better testing enviroment

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//het_gokcandemiralp 04-08-21
+//het_gokcandemiralp 09-08-21
 #include "cpath.h"
 #include "accessories.h"
 
@@ -6,7 +6,23 @@
 
 int main() {
 
-    vector<Point> vec = randomPoints(1, 11, 7);
-    printPoints(closest(vec));
-    return 0;
+    vector<Point> vec = randomPoints(1, 15, 20);
+
+    
+    //cout << "\n"  << totalDistance(vec) << "\n";
+    //printPoints(vec);
+
+
+    // cout << "\n" << totalDistance(closest(vec)) << "\n";
+    //printPoints(closest(vec));
+
+    //cout << doesIntersect(Point(1, 6), Point(6, 6), Point(7, 7), Point(1, 1));
+
+     Mat drawing;
+     drawing = Mat::zeros(Size(1600, 900), CV_8UC3);
+
+
+     michelangelo(drawing, closest(vec));
+     imshow("image", drawing);
+     waitKey(0);
 }
