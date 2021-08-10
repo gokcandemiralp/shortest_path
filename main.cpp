4cf3b48d2d0d57020907a65714c980f9c0afea1c
Gökcan Demiralp <gok.can.13@gmail.com>
2021-08-10 19:28:34 +0300

    improved test cases

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//het_gokcandemiralp 09-08-21
+//het_gokcandemiralp 10-08-21
 #include "cpath.h"
 #include "accessories.h"
 
@@ -6,17 +6,16 @@
 
 int main() {
 
-    vector<Point> vec = randomPoints(1, 15, 20);
+    vector<Point> vec = randomPoints(1, 17, 34);
 
     
-    //cout << "\n"  << totalDistance(vec) << "\n";
+    cout << "\n"  << totalDistance(vec) << "\n";
     //printPoints(vec);
 
 
-    // cout << "\n" << totalDistance(closest(vec)) << "\n";
+    //cout << "\n" << totalDistance(closest(vec)) << "\n";
     //printPoints(closest(vec));
 
-    //cout << doesIntersect(Point(1, 6), Point(6, 6), Point(7, 7), Point(1, 1));
 
      Mat drawing;
      drawing = Mat::zeros(Size(1600, 900), CV_8UC3);
